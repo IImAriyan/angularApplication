76e57956914957c93e91e9cfb1d444ee07a128e0
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-17 16:55:41 +0330

    Add Readme.md File

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig ,NgModule} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {HomeComponent} from "./pages/home/home.component";
+import {ApiService} from "./services/api.service";
+import {BrowserModule} from "@angular/platform-browser";
 import { routes } from './app.routes';
+import {CookieService} from "ngx-cookie-service";
+import {AppComponent} from "./app.component";
+import {provideHttpClient, withFetch} from "@angular/common/http";
+
+
+
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),ApiService,BrowserModule,provideHttpClient(withFetch()),CookieService],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Routes} from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {ImageFilterComponent} from "./pages/image-filter/image-filter.component";
+import {AbcComponent} from "./pages/abc/abc.component";
+import {AverageComponent} from "./pages/average/average.component";
+export const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'} ,
+  {path:'home',component:HomeComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'imageFilter',component:ImageFilterComponent},
+  {path:'abc',component:AbcComponent},
+  {path:'average',component:AverageComponent}
+];
